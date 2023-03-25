ff27c9a5337c55adced6f8433efa47a49a87f207
TPLV <m4xlion@yandex.ru>
2023-03-25 14:07:58 +0300

    WEB. REST-API. Понятие. Делаем простое Rest-api Добавление работы

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 from . import db_session
 from .jobs import Jobs
@@ -11,7 +11,7 @@ blueprint = flask.Blueprint(
 )
 
 
-@blueprint.route('/api/jobs')
+@blueprint.route('/api/jobs', methods=["GET"])
 def get_jobs():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
@@ -39,3 +39,40 @@ def get_one_job(job_id):
                                        "start_date", "end_date", "is_finished"))
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_job():
+    db_sess = db_session.create_session()
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ["id", "team_leader", "job", "work_size",
+                  "collaborators", "start_date",
+                  "is_finished"]):
+        return jsonify({'error': 'Bad request'})
+    if db_sess.query(Jobs).filter(Jobs.id == request.json["id"]).first():
+        return jsonify({'error': 'Id already exists'})
+    jobs = Jobs(
+        id=request.json["id"],
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        start_date = request.json['start_date'],
+        is_finished = request.json['is_finished']
+    )
+    db_sess.add(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/jobs/<int:news_id>', methods=['DELETE'])
+def delete_news(news_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(news_id)
+    if not jobs:
+        return jsonify({'error': 'Not found'})
+    db_sess.delete(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
--- test.py
+++ test.py
@@ -1,6 +1,34 @@
-from requests import get
+from requests import get, post, delete
+
+# Good
+print(post('http://localhost:8080/api/jobs',
+           json={'id': 9,
+                 'team_leader': 2,
+                 'job': 'TEST_API',
+                 'work_size': 10,
+                 "collaborators": "1, 3, 4",
+                 "start_date": "19.03.2023",
+                 'is_finished': False}).json())
+
+# Worst ID
+print(post('http://localhost:8080/api/jobs',
+           json={'id': 4,
+                 'team_leader': 2,
+                 'job': 'TEST_API',
+                 'work_size': 10,
+                 "collaborators": "1, 3, 4",
+                 "start_date": "19.03.2023",
+                 'is_finished': False}).json())
+
+# Нет параметров
+print(post('http://localhost:8080/api/jobs',
+           json={'id': 4,
+                 'team_leader': 2,
+                 'job': 'TEST_API',
+                 'work_size': 10,
+                 "start_date": "19.03.2023",
+                 'is_finished': False}).json())
+
+
 
 print(get('http://localhost:8080/api/jobs').json())
-print(get('http://localhost:8080/api/jobs/1').json())
-print(get('http://localhost:8080/api/jobs/999').json())
-print(get('http://localhost:8080/api/jobs/q').json())
